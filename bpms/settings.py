31d27a103e2ebf1a0dc47b379d4cc6849d0f0614
vaibhavgera2004 <vaibhavgera2004@gmail.com>
2025-08-27 13:24:40 +0530

    Update settings.py

--- bpms/settings.py
+++ bpms/settings.py
@@ -131,3 +131,13 @@ MEDIA_URL = '/services/'
 MEDIA_ROOT = BASE_DIR
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# Add this line 👇
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = '/services/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "services")
